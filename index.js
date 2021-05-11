11b9617bd1aa56053c0b09d7f89caf43b6fff88c
ChaudharyShiva18 <shivachaudhary18@outlook.com>
2021-05-11 18:35:56 +0530

    port changes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-app.listen(3000, function () {
-console.log("listening on 3000");
+app.listen(8000, function () {
+  console.log("listening on 3000");
 });
 app.get("/", (req, res) => {
-res.send("Users Shown");
+  res.send("Users Shown");
 });
 app.get("/delete", (req, res) => {
-res.send("Delete User");
+  res.send("Delete User");
 });
 app.get("/update", (req, res) => {
-res.send("Update User");
+  res.send("Update User");
 });
 app.get("/insert", (req, res) => {
-res.send("Insert User");
+  res.send("Insert User");
 });
