07d3c34b0f7b02b8bd56ad6045c89890b20b3a72
ChaudharyShiva18 <shivachaudhary18@outlook.com>
2021-05-11 19:10:45 +0530

    minor change in port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.listen(8000, function () {
-  console.log("listening on 3000");
+  console.log("listening on 8000");
 });
 app.get("/", (req, res) => {
   res.send("Users Shown");
